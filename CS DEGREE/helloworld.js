2f4e8aa94c63c35b8494a208b565ad749ed08f14
PrayashPanta <43919291+PrayashPanta@users.noreply.github.com>
2019-08-18 20:48:32 -0500

    Add files via upload

---
+++ CS DEGREE/helloworld.js
@@ -0,0 +1,28 @@
+function link(){
+    window.location="firstsem.html"
+}
+
+function homepage(){
+    window.location = "test.html"
+}
+
+function secondsem(){
+    window.location = "secondsem.html"
+}
+
+function thirdsem(){
+    window.location = "thirdsem.html"
+}
+
+function fourthsem(){
+    window.location = "fourthsem.html"
+}
+function fifthsem(){
+    window.location = "fifthsem.html"
+}
+function table(){
+    window.location = "table.html"
+}
+function submit(){
+    alert("Your form is submitted");
+}
